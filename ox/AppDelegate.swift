e7cd2f87f9ef49485b556c64ffc519958a105b27
dominic <hello@dom.voyage>
2019-12-08 20:49:48 -0500

    Fixed UIScene functionality, added pause and play button, etc

--- ox/AppDelegate.swift
+++ ox/AppDelegate.swift
@@ -10,15 +10,21 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    class var sharedInstance: AppDelegate {
+        get {
+            return UIApplication.shared.delegate as! AppDelegate
+        }
+    }
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
         return true
     }
 
     // MARK: UISceneSession Lifecycle
+
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -31,7 +37,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    /*
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let parameters = appRemote.authorizationParameters(from: url)
+        
+        if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = access_token
+            self.accessToken = access_token
+            print("Success! The access token is \(access_token)")
+        } else if let error_description = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            dump(error_description)
+        }
+        return true
+    }*/
+    
+    /*func playSong() {
+        SPTAppRemote.checkIfSpotifyAppIsActive { (isActive) in
+            print("Spotify is active: \(isActive)")
+            self.appRemote = SPTAppRemote(configuration: AppDelegate().configuration, logLevel: .debug)
+            let spotifyInstalled = self.appRemote.authorizeAndPlayURI("spotify:track:69bp2EbF7Q2rqc5N3ylezZ")
+            if !(spotifyInstalled ?? false) {
+                fatalError()
+            } else {
+                print("Spotify is installed! Woo.")
+            }
+        }
+    }*/
 }
-
--- ox/ViewController.swift
+++ ox/ViewController.swift
@@ -8,13 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol PlayerControl: class {
+    func playSong(with uri: String)
+    func pause()
+}
+
+protocol PlayerDelegate: class {
+    func didUpdatePlayer(with player: SPTAppRemotePlayerState)
+    func didConnect()
+    func didDisconnect(with error: Error?)
+    func didFailConnection(with error: Error?)
+    func didFetch(_ image: UIImage, forTrackURI: String)
+}
+
+class PlayerViewController: UIViewController {
+    
+    @IBOutlet weak var trackLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var playlistLabel: UILabel!
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    var isPaused = true
+    weak var playerControl: PlayerControl? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let scene = UIApplication.shared.connectedScenes.first
+        if let sd : SceneDelegate = (scene?.delegate as? SceneDelegate) {
+            sd.viewControllerDelegate = self
+            self.playerControl = sd
+        }
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func didTogglePause() {
+        if isPaused {
+            playerControl?.playSong(with: "20I6sIOMTCkB6w7ryavxtO")
+        } else {
+            playerControl?.pause()
+        }
+        updatePauseButton()
+    }
+    
+    func updatePauseButton() {
+        let systemName = isPaused ? "play.circle.fill" : "pause.circle.fill"
+        pauseButton.setBackgroundImage(UIImage(systemName: systemName), for: UIControl.State())
+    }
 }
 
+extension PlayerViewController: PlayerDelegate {
+    func didFetch(_ image: UIImage, forTrackURI: String) {
+        albumImageView.image = image
+    }
+    
+    func didDisconnect(with error: Error?) {
+        print("disconnected")
+    }
+    
+    func didFailConnection(with error: Error?) {
+        print("failed connection")
+    }
+    
+    func didConnect() {
+        print("connected")
+    }
+    
+    func didUpdatePlayer(with player: SPTAppRemotePlayerState) {
+        print(player.isPaused ? "⏸" : "▶️")
+        print(player.contextTitle)
+        print(player.playbackPosition)
+        print(player.track.name)
+        trackLabel.text = player.track.name
+        playlistLabel.text = player.contextTitle
+        artistLabel.text = player.track.artist.name
+        albumLabel.text = player.track.album.name
+        //albumImageView.image = player.track
+        
+        isPaused = player.isPaused
+        updatePauseButton()
+    }
+    
+}
