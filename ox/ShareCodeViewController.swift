b11f80542db47c2546c11da9fe5f1b0ca9c9c45a
dominic <hello@dom.voyage>
2019-12-08 23:02:12 -0500

    Create QR code screen

---
+++ ox/ShareCodeViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ShareCodeViewController.swift
+//  ox
+//
+//  Created by Dominic Holmes on 12/8/19.
+//  Copyright © 2019 Dominic Holmes. All rights reserved.
+//
+
+import UIKit
+
+class ShareCodeViewController: UIViewController {
+    
+    @IBOutlet weak var qrImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        generateCodes()
+    }
+    
+    func generateCodes() {
+        // Get define string to encode
+        let myString = "https://ox.fm/1234"
+        // Get data from the string
+        let data = myString.data(using: String.Encoding.ascii)
+        // Get a QR CIFilter
+        guard let qrFilter = CIFilter(name: "CIQRCodeGenerator") else { return }
+        // Input the data
+        qrFilter.setValue(data, forKey: "inputMessage")
+        // Get the output image
+        guard let qrImage = qrFilter.outputImage else { return }
+        // Scale the image
+        let transform = CGAffineTransform(scaleX: 10, y: 10)
+        let scaledQrImage = qrImage.transformed(by: transform)
+        // Invert the colors
+        guard let colorInvertFilter = CIFilter(name: "CIColorInvert") else { return }
+        colorInvertFilter.setValue(scaledQrImage, forKey: "inputImage")
+        guard let outputInvertedImage = colorInvertFilter.outputImage else { return }
+        // Replace the black with transparency
+        guard let maskToAlphaFilter = CIFilter(name: "CIMaskToAlpha") else { return }
+        maskToAlphaFilter.setValue(outputInvertedImage, forKey: "inputImage")
+        guard let outputCIImage = maskToAlphaFilter.outputImage else { return }
+        // Do some processing to get the UIImage
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return }
+        let processedImage = UIImage(cgImage: cgImage)
+        
+        qrImageView.image = processedImage
+    }
+}
